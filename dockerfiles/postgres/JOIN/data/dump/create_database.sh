7828f671c0e2d801b4ba38849cc6756822bb1335
dev <dev@Hermes.devng.fr>
2014-11-10 17:53:41 +0100

    Création de la base de donnée dans le Dockerfile

--- dockerfiles/postgres/JOIN/data/dump/create_database.sh
+++ dockerfiles/postgres/JOIN/data/dump/create_database.sh
@@ -5,6 +5,9 @@ $POSTGRES --single -E <<EOSQL
 CREATE DATABASE wsd_postgres_database_name
 CREATE USER wsd_postgres_user WITH PASSWORD 'wsd_postgres_password'
 CREATE ROLE developer
-GRANT developer TO wsd_postgres_user
+GRANT developer TO dev
 GRANT ALL PRIVILEGES ON DATABASE wsd_postgres_database_name TO developer
 EOSQL
+
+exec psql -U dev wsd_postgres_database_name < /wsd_postgres/dbexport.pgsql;
+#exec psql -X -U dev wsd_postgres_database_name --set ON_ERROR_STOP=on -c "SELECT * FROM pg_roles;"
