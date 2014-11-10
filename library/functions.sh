0f47f9f0eb2e9c0a32795634085dc5ec19341c3c
dev <dev@Hermes.devng.fr>
2014-11-10 09:59:57 +0100

    add functions : compare version

--- library/functions.sh
+++ library/functions.sh
@@ -53,3 +53,29 @@ function subShow(){
     echo '   ------------------------------------------------------'
     wait;
 }
+
+function version_compare {
+
+IFS='.' read -ra ver1 <<< "$1"
+IFS='.' read -ra ver2 <<< "$2"
+
+[[ ${#ver1[@]} -gt ${#ver2[@]} ]] && till=${#ver1[@]} || till=${#ver2[@]}
+
+for ((i=0; i<${till}; i++)); do
+
+    local num1; local num2;
+
+    [[ -z ${ver1[i]} ]] && num1=0 || num1=${ver1[i]}
+    [[ -z ${ver2[i]} ]] && num2=0 || num2=${ver2[i]}
+
+    if [[ $num1 -gt $num2 ]]; then
+        echo ">"; return 0
+    elif
+       [[ $num1 -lt $num2 ]]; then
+        echo "<"; return 0
+    fi
+done
+
+echo "="; return 0
+}
+
