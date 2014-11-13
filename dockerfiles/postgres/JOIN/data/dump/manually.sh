746dc61fdd840e728c5b22844e895d6787b403b4
dev <dev@Hermes.devng.fr>
2014-11-13 21:41:45 +0100

    Création de la base de donnée et de l'utilisateur

--- dockerfiles/postgres/JOIN/data/dump/manually.sh
+++ dockerfiles/postgres/JOIN/data/dump/manually.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+gosu postgres createdb wsd_postgres_database_name;
+# Todo : ajouter un role "developper"
+gosu postgres createuser wsd_postgres_user;
+psql -X -U postgres --set ON_ERROR_STOP=no -c "ALTER USER wsd_postgres_user WITH PASSWORD 'wsd_postgres_password';";
+
 psql -X -U postgres --set ON_ERROR_STOP=no -c "SELECT * FROM pg_roles;";
 psql -U postgres wsd_postgres_database_name < /wsd_postgres/dbexport.pgsql;
