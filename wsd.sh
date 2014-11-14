8061a8d898dacb62edd317dfc5804ca6f7c9c964
dev <dev@Hermes.devng.fr>
2014-11-14 11:42:03 +0100

    Répertoire d'arborescence de base

--- wsd.sh
+++ wsd.sh
@@ -3,6 +3,14 @@
 dockerDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/";
 wsd_action=${1};
 
+mkdir -p ${dockerDir}backups;
+mkdir -p ${dockerDir}dockerfiles/nginx/LINK/log;
+mkdir -p ${dockerDir}dockerfiles/nginx/LINK/htpasswds;
+mkdir -p ${dockerDir}dockerfiles/nginx/LINK/www;
+mkdir -p ${dockerDir}dockerfiles/nginx/LINK/sites-enabled;
+mkdir -p ${dockerDir}dockerfiles/piwik;
+mkdir -p ${dockerDir}logs;
+
 if [[ ${wsd_action} == "install" ]]; then
     source "${dockerDir}library/install.sh";
 elif [[ ${wsd_action} == "restart" ]]; then
